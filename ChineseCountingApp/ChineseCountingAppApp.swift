e65a91058e916dd6f2e7bbee05b98d790ae657f0
kamila <ksalimzh@gmail.com>
2025-03-28 14:25:06 +0500

    Initial Commit

---
+++ ChineseCountingApp/ChineseCountingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChineseCountingAppApp.swift
+//  ChineseCountingApp
+//
+//  Created by kamila on 28.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChineseCountingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
