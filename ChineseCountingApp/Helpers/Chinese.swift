29f3973a87fc882daf793651a548ec710941f6f1
kamila <ksalimzh@gmail.com>
2025-03-28 21:34:25 +0500

    unit testing TDD

---
+++ ChineseCountingApp/Helpers/Chinese.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+class Chinese {
+    static func num99toChinese(_ num: Int) -> (chinese: String, pinyin: String)? {
+        guard num >= 0 && num <= 99 else { return nil }
+        return nil
+    }
+}
